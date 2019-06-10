ecca1b8a4702504b9b532f6bdd07ff98f25274ee
enitsed <sllki1@naver.com>
2019-06-10 22:17:32 +0900

    still confused using this object binding

--- front/src/components/Basic/BasicNavBar.js
+++ front/src/components/Basic/BasicNavBar.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import BasicButton from './BasicButton';
 import styled from 'styled-components';
+import axios from 'axios';
 
 class NavBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLogin : false,
+            user : {},
         }
     }
 
+    requestUserData = () => {
+        let usersQuery = "{user(user_seq : 1){user_seq,user_name,gender,user_status}}";
+        
+        return new Promise(
+            (resolve, reject) => {
+                axios.get('/graphql', {
+                    params : {
+                        query : usersQuery
+                    }
+                })
+                .then((response) => {
+                    resolve(response.data);
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            }
+        ).then( data => { this.setState({user : data.data.user}); });
+
+    }
+
     login = () => {
+        this.requestUserData();
+        console.log(this.state.user);
         this.setState({isLogin : true});
     }
     
@@ -31,7 +55,7 @@ class NavBar extends Component {
         } else {
             return (
                 <div className = { this.props.className } >
-                    <BasicButton clickHandler={this.logout}>로그아웃</BasicButton>
+                    <BasicButton clickHandler={this.logout}>{this.state.user.user_name}님 로그아웃</BasicButton>
                     <BasicButton>마이페이지</BasicButton>
                 </div>
             )
