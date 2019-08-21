b4d094609f0f04ca3a058fc5895519edcc937209
enitsed <sllki1@naver.com>
2019-08-21 19:51:10 +0900

    working on desigining

--- front/src/components/Album/AlbumContainer.js
+++ front/src/components/Album/AlbumContainer.js
@@ -3,30 +3,66 @@ import { Card } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 class CustomContainer extends Component {
-    render () {
-        return (
-            <Card.Group itemsPerRow={4} className={this.props.className}>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-                <Card color="olive" image="http://pngimg.com/uploads/almond/almond_PNG72.png"/>
-            </Card.Group>
-        );
-    }
+  render() {
+    return (
+      <Card.Group itemsPerRow={4} className={this.props.className}>
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+        <Card
+          color="olive"
+          image="http://pngimg.com/uploads/almond/almond_PNG72.png"
+        />
+      </Card.Group>
+    );
+  }
 }
 
 const AlbumContainer = styled(CustomContainer)`
-    min-width : 25vw;
-    max-width : 75vw;
-    margin-top : 15px !important;
-`
+  min-width: 25vw;
+  max-width: 75vw;
+  margin-top: 15px !important;
+`;
 
 export default AlbumContainer;
