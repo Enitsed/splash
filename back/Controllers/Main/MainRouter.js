c196f41a7aa0ab6c60acbddf68ab9cf5a664479a
enitsed <sllki1@naver.com>
2019-09-21 02:08:38 +0900

    set view engine for express app and added react build file

--- back/Controllers/Main/MainRouter.js
+++ back/Controllers/Main/MainRouter.js
@@ -1,5 +1,7 @@
 const graphql = require('../Graphql/graphql');
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 module.exports = class Routes {
     
@@ -11,8 +13,21 @@ module.exports = class Routes {
         //Throws if no instance of express was passed
         if (app == null) throw new Error("You must provide an instance of express");
 
+		// set view path
+		app.set('views', './src');
+
+		// set app view Directory and template engine
+		app.set('view engine', 'ejs');
+		app.engine('html', ejs.renderFile);
+		app.use(express.static('./src'));
+
         //Registers the base GraphQLi base endpoint
         app.use('/graphql', graphql);
+
+		// render React router
+		app.get('/', (req, res) => {
+			res.render('index');
+		});
 
         app.get('/user', function(req, res) {
             res.send("he");
