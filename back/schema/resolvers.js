4047a62c7958bd1f418cb16a121a5943a385fd55
enitsed <sllki1@naver.com>
2020-04-24 19:18:48 +0900

    in a quarantine

--- back/schema/resolvers.js
+++ back/schema/resolvers.js
@@ -14,42 +14,15 @@ const resolvers = {
     },
 
     userLogin(_, { userLoginInput }, context) {
-      const { user_id, user_password } = userLoginInput;
-      if (!user_id) {
-        return { login_history: { login_status: "type user id" } };
-      }
-
-      if (!user_password) {
-        return { login_history: { login_status: "type user password" } };
-      }
-
-      return AuthService.getUserInfo(_, { user_id, user_password }, context);
-    }
+      return AuthService.getUserInfo(_, userLoginInput, context);
+    },
   },
   Mutation: {
     async addUser(_, { userSignUpInput }) {
-      const { user_id } = userSignUpInput;
-
-      await User.findMatching(_, { user_id })
-        .then(res => {
-          if (res.shift() || res.length > 0) {
-            throw new Error("user id already exists");
-          }
-        })
-        .catch(err => {
-          throw new Error(err);
-        });
-
-      const fields = {
-        ...userSignUpInput,
-        user_status: Constants.USER_STATUS.ACTIVE,
-        create_time: new Date("YYYY-MM-dd HH:mm:ss")
-      };
-
-      return await User.createEntry(_, fields);
-    }
+      return AuthService.signUpUserInfo(_, userSignUpInput);
+    },
   },
-  Date: TimeStamp
+  Date: TimeStamp,
 };
 
 module.exports = resolvers;
