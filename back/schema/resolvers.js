c1b686023096ffa91e989d00957b30e4146ad50c
enitsed <sllki1@naver.com>
2020-06-04 20:41:55 +0900

    login token

--- back/schema/resolvers.js
+++ back/schema/resolvers.js
@@ -5,19 +5,28 @@ const TimeStamp = require("../models/common/GraphQLTimestamp");
 const resolvers = {
   Query: {
     user(_, { user_seq }, context) {
+      if (!context.user || !context.user.isAdmin) {
+        throw new Error("admin only");
+      }
       return User.getByUserSeq(_, { user_seq }, context.user);
     },
 
     users(_, fields, context) {
+      if (!context.user || !context.user.isAdmin) {
+        throw new Error("admin only");
+      }
       return User.findMatching(_, fields, context.user);
     },
 
     userLogin(_, { userLoginInput }, context) {
-      return AuthService.getUserInfo(_, userLoginInput, context);
+      return AuthService.getUserInfo(_, userLoginInput).userData;
     },
   },
   Mutation: {
     async addUser(_, { userSignUpInput }) {
+      if (!context.user || !context.user.isAdmin) {
+        throw new Error("admin only");
+      }
       return AuthService.signUpUserInfo(_, userSignUpInput);
     },
   },
