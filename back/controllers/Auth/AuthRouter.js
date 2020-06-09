0dc4438a47cc45dec11e252405c349be8b67281a
enitsed <sllki1@naver.com>
2020-06-09 19:31:11 +0900

    Auth Settings

--- back/controllers/Auth/AuthRouter.js
+++ back/controllers/Auth/AuthRouter.js
@@ -1,5 +1,6 @@
 const AuthService = require("../../services/authService");
 const jwt = require("jsonwebtoken");
+const Constants = require("../../models/common/Constants");
 
 module.exports = class AuthRouter {
   constructor(app) {
@@ -37,15 +38,17 @@ module.exports = class AuthRouter {
             { user: { ...data } },
             req.app.get("jwt-secret"),
             {
-              expiresIn: "1h",
-              issuer: "splash.com",
-              subject: "userData",
+              expiresIn: Constants.JWT_TOKEN._EXPIRES_TIME,
+              issuer: Constants.JWT_TOKEN._ISSUER,
+              subject: Constants.JWT_TOKEN._SUBJECT,
             }
           );
 
           req.session.user = data;
           return res
-            .cookie("user", encodedUserData, { maxAge: 10800 })
+            .cookie("user", encodedUserData, {
+              maxAge: Constants.COOKIE._MAX_AGE,
+            })
             .json(req.session.user);
         })
         .catch((err) => {
@@ -54,49 +57,60 @@ module.exports = class AuthRouter {
     });
 
     // login mapping
-    app.post("/login", async (req, res) => {
+    app.post("/login", (req, res) => {
       if (req.session.user && req.cookies.user) {
         return res.redirect("/cookieLogin");
       }
 
       const { user_id, user_password } = req.body.variables;
 
-      await AuthService.getUserInfo(
+      AuthService.getUserInfo(
         { ip: req.ip },
         {
           user_id,
           user_password,
         }
-      ).then((data) => {
-        const encodedUserData = jwt.sign(
-          { user: { ...data } },
-          req.app.get("jwt-secret"),
-          {
-            expiresIn: "1h",
-            issuer: "splash.com",
-            subject: "userData",
-          }
-        );
-        req.session.user = data;
-        return res
-          .cookie("user", encodedUserData, { maxAge: 10800 })
-          .json(req.session.user);
-      });
+      )
+        .then((data) => {
+          const encodedUserData = jwt.sign(
+            { user: { ...data } },
+            req.app.get("jwt-secret"),
+            {
+              expiresIn: Constants.JWT_TOKEN._EXPIRES_TIME,
+              issuer: Constants.JWT_TOKEN._ISSUER,
+              subject: Constants.JWT_TOKEN._SUBJECT,
+            }
+          );
+          req.session.user = data;
+          // 임시로 관리자 지정 후에 디비 테이블 만들어서 관리할것
+          // req.session.user.isAdmin = true;
+
+          return res
+            .cookie("user", encodedUserData, {
+              maxAge: Constants.COOKIE._MAX_AGE,
+            })
+            .json(req.session.user);
+        })
+        .catch((err) => console.error(err));
     });
 
     // cookie login mapping
-    app.post("/cookieLogin", async (req, res) => {
+    app.post("/cookieLogin", (req, res) => {
       if (req.session.user && req.cookies.user) {
+        const userJwt = jwt.verify(req.cookies.user, app.get("jwt-secret"));
+        if (req.session.user.user_password !== userJwt.user.user_password) {
+          throw new Error("세션 유저 데이터와 일치하지 않는 쿠키 데이터");
+        }
         return res.json(req.session.user);
       }
 
       return res.redirect("/");
     });
 
-    // cookie login mapping
-    app.post("/logout", async (req, res) => {
+    // expire session cookie
+    app.post("/logout", (req, res) => {
       req.session.user = null;
-      res.cookie("user", null, { maxAge: 10 });
+      res.cookie("user", null, { maxAge: 1 });
 
       return res.redirect("/");
     });
