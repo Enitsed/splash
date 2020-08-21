88e0fe233e2ebd6fb69043f8177571470aa83397
enitsed <sllki1@naver.com>
2020-08-22 05:57:21 +0900

    config fix

---
+++ back/config/SMTPConfig.js
@@ -0,0 +1,15 @@
+if (process.env.STAGE === "development") {
+  require("dotenv").config();
+}
+
+const SMTPConfig = {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_AUTH_USER,
+    pass: process.env.SMTP_AUTH_PASS,
+  },
+};
+
+module.exports = SMTPConfig;
